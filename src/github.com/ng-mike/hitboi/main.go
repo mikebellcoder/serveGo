32ab7b9e34664d8463678a45fdb5c272ead5b50d
ngMike <mikebell@scientology.net>
2017-05-20 09:51:27 -0700

    First Self Commit

--- src/github.com/ng-mike/hitboi/main.go
+++ src/github.com/ng-mike/hitboi/main.go
@@ -8,6 +8,9 @@ func main() {
 		fmt.Printf("%v\n", count)
 		fmt.Printf("%b\n", count)
 		fmt.Printf("%#x\n", count)
+		if count == 0 {
+			fmt.Printf("HitBoi")
+		}
 		time.Sleep(time.Second)
 	}
 }
