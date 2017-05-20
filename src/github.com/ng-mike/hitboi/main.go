47455038a7a96b41abfe974ce6eeff8214cf43d0
ngMike <mikebell@scientology.net>
2017-05-20 09:22:37 -0700

    I got all the hits boi!

--- src/github.com/ng-mike/hitboi/main.go
+++ src/github.com/ng-mike/hitboi/main.go
@@ -1,11 +1,13 @@
 package main
 
 import "fmt"
+import "time"
 
 func main() {
 	for count := 10; count >= 0; count-- {
-		fmt.Printf("%v", count)
-		fmt.Printf("%b", count)
-		fmt.Printf("%#x", count)
+		fmt.Printf("%v\n", count)
+		fmt.Printf("%b\n", count)
+		fmt.Printf("%#x\n", count)
+		time.Sleep(time.Second)
 	}
 }
