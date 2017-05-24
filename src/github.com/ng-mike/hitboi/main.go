d7358a33c94533b34069e8c4a5d18b6126251fde
ngMike <mikebell@scientology.net>
2017-05-24 10:19:35 -0700

    testing scope

--- src/github.com/ng-mike/hitboi/main.go
+++ src/github.com/ng-mike/hitboi/main.go
@@ -1,21 +1,30 @@
 package main
 
 import "fmt"
-import "time"
+
+var pgkScope = 0
 
 func main() {
-	for count := 20; count >= 0; count-- {
-		fmt.Printf("%v\n", count)
-		fmt.Printf("%b\n", count)
-		fmt.Printf("%#x\n", count)
-		if count == 0 {
-<<<<<<< HEAD
-			fmt.Printf("Baby!")
-=======
-			fmt.Printf("HitBoi")
-			fmt.Printf("HitBoi")
->>>>>>> save from core on Self Branch
-		}
-		time.Sleep(time.Second)
+	incPgkScope := func() int {
+		pgkScope++
+		return pgkScope
 	}
+	funcScope := incPgkScope()
+	count := func() int {
+		funcScope++
+		return funcScope
+	}
+
+	for i := 0; i < 10; i++ {
+		fmt.Printf("pgkScope value is: %v\n", pgkScope)
+		fmt.Printf("funcScope value is: %v\n", funcScope)
+		fmt.Printf("%v\n", count())
+
+	}
+
 }
+
+/* badTry is purposely trying to access funcScope variable which is out of scope! */
+// func badTry() {
+// 	fmt.Println(funcScope)
+// }
