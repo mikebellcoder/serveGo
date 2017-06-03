b77e9d53ae8b2eb88402c6207fd358954b3d2a96
mikeb <mikeb>
2017-06-03 05:47:41 +0100

    Hitboi

--- src/github.com/ng-mike/hitboi/main.go
+++ src/github.com/ng-mike/hitboi/main.go
@@ -1,40 +1,14 @@
-<<<<<<< HEAD
 package main
 
 import "fmt"
 import "time"
 
 func main() {
-	for count := 20; count >= 0; count-- {
+	for count := 10; count >= 0; count-- {
 		fmt.Printf("%v\n", count)
-		fmt.Printf("%b\n", count)
-		fmt.Printf("%#x\n", count)
-		if count == 0 {
-			fmt.Printf("Take off!")
+		if count == 0 {			
+			fmt.Printf("HitBoi!")
 		}
 		time.Sleep(time.Second)
 	}
 }
-=======
-package main
-
-import "fmt"
-import "time"
-
-func main() {
-	for count := 20; count >= 0; count-- {
-		fmt.Printf("%v\n", count)
-		fmt.Printf("%b\n", count)
-		fmt.Printf("%#x\n", count)
-		if count == 0 {
-<<<<<<< HEAD
-			fmt.Printf("Baby!")
-=======
-			fmt.Printf("HitBoi")
-			fmt.Printf("HitBoi")
->>>>>>> save from core on Self Branch
-		}
-		time.Sleep(time.Second)
-	}
-}
->>>>>>> self
