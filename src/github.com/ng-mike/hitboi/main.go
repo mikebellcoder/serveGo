89e16a3e8a72956d6a76424b5a5cded99250708c
mikeb <mikeb>
2017-06-03 17:29:49 +0100

    Changed hitboi main.go

--- src/github.com/ng-mike/hitboi/main.go
+++ src/github.com/ng-mike/hitboi/main.go
@@ -1,34 +1,27 @@
 package main
 
-import "fmt"
-
-var pgkScope = 0
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-<<<<<<< HEAD
-	for count := 10; count >= 0; count-- {
-		fmt.Printf("%v\n", count)
-		if count == 0 {			
-			fmt.Printf("HitBoi!")
+	hour := time.Now().Hour()
+
+	var getGreeting = func(t int) string {
+		var greet string
+		if t < 12 {
+			greet = "Good morning."
+		} else if t > 12 {
+			greet = "Good Afternoon."
+
+		} else if t < 18 {
+			greet = "Good Evening."
 		}
-		time.Sleep(time.Second)
-=======
-	incPgkScope := func() int {
-		pgkScope++
-		return pgkScope
-	}
-	funcScope := incPgkScope()
-	count := func() int {
-		funcScope++
-		return funcScope
+		return greet
 	}
 
-	for i := 0; i < 10; i++ {
-		fmt.Printf("pgkScope value is: %v\n", pgkScope)
-		fmt.Printf("funcScope value is: %v\n", funcScope)
-		fmt.Printf("%v\n", count())
-
->>>>>>> 748fcef5a6ceab8479e415f26bcbae737bd4858a
-	}
+	greeting := getGreeting(hour)
+	fmt.Println(greeting)
 
 }
